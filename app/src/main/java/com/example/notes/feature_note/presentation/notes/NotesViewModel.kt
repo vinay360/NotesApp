bbc7cc5b0f0a64eeb7cd30af3910f34bf39695eb
Vinay Agarwal <vinayagarwal360@gmail.com>
2023-08-07 21:27:03 +0530

    Solved notes not loading error

--- app/src/main/java/com/example/notes/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/notes/feature_note/presentation/notes/NotesViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.notes.feature_note.domain.use_case.UseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,32 +17,40 @@ class NotesViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _state = mutableStateOf(NotesState())
-    val state : State<NotesState> = _state
+    val state: State<NotesState> = _state
 
     init {
-        noteUseCases.getNotes()
+        noteUseCases.getNotes(noteOrder = state.value.noteOrder).onEach { notes ->
+            _state.value = state.value.copy(notes = notes)
+        }.launchIn(viewModelScope)
     }
+
     fun onEvent(event: NotesEvent) {
-        when(event) {
+        when (event) {
             is NotesEvent.DeleteNote -> {
                 viewModelScope.launch {
                     noteUseCases.deleteNote(event.note)
                     _state.value = state.value.copy(recentlyDeleteNote = event.note)
                 }
             }
+
             is NotesEvent.Order -> {
                 _state.value = state.value.copy(
                     notes = noteUseCases.orderNotes(state.value.notes, event.noteOrder),
-                    noteOrder = event.noteOrder)
+                    noteOrder = event.noteOrder
+                )
             }
+
             NotesEvent.RestoreNote -> {
                 viewModelScope.launch {
-                    noteUseCases.addNote(state.value.recentlyDeleteNote?:return@launch)
+                    noteUseCases.addNote(state.value.recentlyDeleteNote ?: return@launch)
                     _state.value = state.value.copy(recentlyDeleteNote = null)
                 }
             }
+
             NotesEvent.ToogleOrderSection -> {
-                _state.value = state.value.copy(isOrderSectionVisible = !state.value.isOrderSectionVisible)
+                _state.value =
+                    state.value.copy(isOrderSectionVisible = !state.value.isOrderSectionVisible)
             }
         }
     }
